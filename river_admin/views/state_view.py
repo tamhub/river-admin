16dad69b2687c3d4cea3ec212689830cb03c41a3
mohamedAbdElhameed <mohamedabdelhameed34@gmail.com>
2024-05-14 20:34:28 +0300

    feat (river_admin): Refactor state view and enhance functionality
    This commit renames some methods for better readability in state_view.py and adds new helper functions for more comprehensive functionality. It also modifies the delete_state_from_workflow method to include functions for updating instances with their previous state, resetting initial state for instances, and deleting state transitions.

--- river_admin/views/state_view.py
+++ river_admin/views/state_view.py
@@ -1,26 +1,28 @@
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
-from river.models import State
+from river.models.state import State
+from river.models.workflow import Workflow
+from river.models.transitionapproval import TransitionApproval
 from river.models.transitionmeta import TransitionMeta
-
 from river_admin.views import get, post, delete
 from river_admin.views.serializers import StateDto, CreateStateDto
 
 
 @get(r'state/get/<int:pk>/')
-def get_it(request, pk):
+def get_state(request, pk):
     state = get_object_or_404(State.objects.all(), pk=pk)
     return Response(StateDto(state).data, status=HTTP_200_OK)
 
 
 @get(r'state/list/')
-def list_it(request):
-    return Response(StateDto(State.objects.all(), many=True).data, status=HTTP_200_OK)
+def list_states(request):
+    states = State.objects.all()
+    return Response(StateDto(states, many=True).data, status=HTTP_200_OK)
 
 
 @post(r'state/create/')
-def create_it(request):
+def create_state(request):
     create_state_request = CreateStateDto(data=request.data)
     if create_state_request.is_valid():
         state = create_state_request.save()
@@ -30,7 +32,7 @@ def create_it(request):
 
 
 @delete(r'state/delete/<int:pk>/')
-def delete_it(request, pk):
+def delete_state(request, pk):
     state = get_object_or_404(State.objects.all(), pk=pk)
     state.delete()
     return Response(status=HTTP_200_OK)
@@ -38,17 +40,62 @@ def delete_it(request, pk):
 
 @delete(r'state/delete/<int:workflow_id>/<int:pk>/')
 def delete_state_from_workflow(request, workflow_id, pk):
-    state = get_state(pk)
-    delete_transitions_from_source(workflow_id, state)
-    delete_transitions_from_destination(workflow_id, state)
+    state = get_state_by_pk(pk)
+    workflow = get_workflow_by_pk(workflow_id)
+    update_instances_with_previous_state(workflow, state)
+    reset_initial_state_for_instances(workflow, state)
+    delete_state_transitions(workflow_id, state)
     return Response(status=HTTP_200_OK)
 
 
-def get_state(pk):
+# Helper functions
+
+def get_state_by_pk(pk):
     """
     Get a State based on its primary key (pk)
     """
     return get_object_or_404(State.objects.all(), pk=pk)
+
+
+def get_workflow_by_pk(workflow_id):
+    """
+    Get a Workflow based on its primary key (workflow_id)
+    """
+    return get_object_or_404(Workflow, pk=workflow_id)
+
+
+def update_instances_with_previous_state(workflow, state):
+    """
+    Update instances related to the workflow, setting their state to the previous state
+    """
+    model_class = workflow.content_type.model_class()
+    for instance in model_class.objects.filter(**{workflow.field_name: state}):
+        previous_transition = TransitionApproval.objects.filter(
+            workflow=workflow,
+            transition__destination_state=state,
+            workflow_object_id=instance.pk
+        ).order_by('-transaction_date').first()
+
+        if previous_transition:
+            previous_state = previous_transition.transition.source_state
+            setattr(instance, workflow.field_name, previous_state)
+            instance.save()
+
+
+def reset_initial_state_for_instances(workflow, state):
+    """
+    Reset the state of instances related to the workflow to the initial state
+    """
+    model_class = workflow.content_type.model_class()
+    model_class.objects.filter(**{workflow.field_name: state}).update(**{workflow.field_name: workflow.initial_state})
+
+
+def delete_state_transitions(workflow_id, state):
+    """
+    Delete TransitionMeta instances related to the state and workflow
+    """
+    delete_transitions_from_source(workflow_id, state)
+    delete_transitions_from_destination(workflow_id, state)
 
 
 def delete_transitions_from_source(workflow_id, state):
